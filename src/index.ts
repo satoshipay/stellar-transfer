bce0a250a8a66ac233b7e55fb6d1732c2243ecdb
Marcel Ebert <marcel@wolfgang-ebert.de>
2019-07-24 15:07:08 +0200

    Add withdrawal statemachine

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export * from "./convenience"
 export * from "./resolver"
 export * from "./responses"
 export * from "./transfer-server"
+
+import * as withdrawal from "./statemachine"
+export { withdrawal }
