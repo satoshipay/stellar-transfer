d5f983d4d289d9f3e5fb3d0e3d4ffd2f6257b6c9
Andy Wermke <andy.dev@next-step-software.com>
2019-11-12 17:12:39 +0100

    `id` prop is mandatory in KYC responses and `interactive_deposit` does not exist anymore

--- src/kyc.ts
+++ src/kyc.ts
@@ -1,12 +1,6 @@
 export interface KYCInteractiveResponse {
   /** The anchor's internal ID for this deposit / withdrawal request. Can be passed to the `/transaction` endpoint to check status of the request. */
-  id?: string
-  /**
-   * Flag indicating that depositing is also handled in the anchor's interactive customer info flow.
-   * The wallet need not make additional requests to /deposit to complete the deposit.
-   * Defaults to false. Only relevant for responses to /deposit requests.
-   */
-  interactive_deposit?: boolean
+  id: string
   /** URL hosted by the anchor. The wallet should show this URL to the user either as a popup or an iframe. */
   url: string
   type: "interactive_customer_info_needed"
